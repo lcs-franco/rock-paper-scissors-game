3ca74790a9033ceac543218925ef80a8d15d87f5
Lucas Franco <lcs.franco556@gmail.com>
2024-12-17 19:13:19 -0300

    refactor: remove unnecessary function

--- src/ui/pages/Play.tsx
+++ src/ui/pages/Play.tsx
@@ -3,29 +3,25 @@ import { useGameProvider } from '@app/contexts/GameProvider'
 export default function Play() {
   const { chooseOption } = useGameProvider()
 
-  const handleOptionClick = (option) => {
-    chooseOption(option)
-  }
-
   return (
     <div className="flex flex-col items-center mt-20">
       <div className="relative w-96 h-96">
         <img
           src="/fistE.png"
           alt="Rock"
-          onClick={() => handleOptionClick('rock')}
+          onClick={() => chooseOption('rock')}
           className="absolute top-0 left-1/2 transform -translate-x-1/2 w-48 h-48"
         />
         <img
           src="/handE.png"
           alt="Paper"
-          onClick={() => handleOptionClick('paper')}
+          onClick={() => chooseOption('paper')}
           className="absolute bottom-0 left-0 transform -translate-x-20 translate-y-20 w-48 h-48"
         />
         <img
           src="/scissorsE.png"
           alt="Scissors"
-          onClick={() => handleOptionClick('scissors')}
+          onClick={() => chooseOption('scissors')}
           className="absolute bottom-0 right-0 transform translate-x-20 translate-y-20 w-48 h-48"
         />
       </div>

87fa98e775103616c3e4931ffb145d84b2700c3c
Lucas Franco <lcs.franco556@gmail.com>
2024-12-19 16:09:24 -0300

    refactor: better implementation of children component

--- src/ui/pages/Play.tsx
+++ src/ui/pages/Play.tsx
@@ -11,23 +11,26 @@ export default function Play() {
     <div className="flex flex-col items-center mt-20">
       <div className="relative w-96 h-96">
         <ImageTooltip
-          children={<RockIcon />}
           tooltipText="Rock"
           onClick={() => chooseOption('rock')}
           className="absolute top-0 left-1/2 transform -translate-x-1/2 w-48 h-48"
-        />
+        >
+          <RockIcon />
+        </ImageTooltip>
         <ImageTooltip
-          children={<PaperIcon />}
           tooltipText="Paper"
           onClick={() => chooseOption('paper')}
           className="absolute bottom-0 left-0 transform -translate-x-20 translate-y-20 w-48 h-48"
-        />
+        >
+          <PaperIcon />
+        </ImageTooltip>
         <ImageTooltip
-          children={<ScissorsIcon />}
           tooltipText="Scissors"
           onClick={() => chooseOption('scissors')}
           className="absolute bottom-0 right-0 transform translate-x-20 translate-y-20 w-48 h-48"
-        />
+        >
+          <ScissorsIcon />
+        </ImageTooltip>
       </div>
       <p className="mt-32 mb-2 text-lg text-center"></p>
     </div>

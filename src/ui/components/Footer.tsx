c428c704b092910e7f6a3528aaaca1cb10e881a8
Lucas Franco <lcs.franco556@gmail.com>
2024-12-19 17:20:06 -0300

    refactor: improve style

--- src/ui/components/Footer.tsx
+++ src/ui/components/Footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer className=" bg-gray-800 text-white flex justify-between p-4  py-7 text-base mt-28">
+    <footer className=" bg-gray-800 text-white flex justify-between p-4 py-4 text-base">
       <p>Desenvolvido por Lucas Franco</p>
       <button>Ver regras</button> {/*TODO: Modal de regras*/}
     </footer>

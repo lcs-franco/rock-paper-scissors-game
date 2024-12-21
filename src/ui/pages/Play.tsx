0544244398f7c72fa7bc70e37310046c2aa1fe7f
Lucas Franco <lcs.franco556@gmail.com>
2024-12-20 22:21:29 -0300

    feat: add handleSubmit function and navigate function

--- src/ui/pages/Play.tsx
+++ src/ui/pages/Play.tsx
@@ -1,14 +1,23 @@
-import { useGameProvider } from '@app/contexts/GameProvider'
+import { CloverIcon } from 'lucide-react'
+import { useNavigate } from 'react-router-dom'
+
+import { Choice, useGameProvider } from '@app/contexts/GameProvider'
 import { PaperIcon } from '@ui/components/icons/PaperIcon'
 import { RockIcon } from '@ui/components/icons/RockIcon'
 import { ScissorsIcon } from '@ui/components/icons/ScissorsIcon'
 import { ImageTooltip } from '@ui/components/ImageTooltip'
 import { Button } from '@ui/components/ui/Button'
 import { Spinner } from '@ui/components/ui/Spinner'
-import { CloverIcon } from 'lucide-react'
 
 export default function Play() {
   const { chooseOption, calculating, randomChoose } = useGameProvider()
+  const navigate = useNavigate()
+
+  const handleSubmit = async (option?: Choice, random = false) => {
+    if (!random) await chooseOption(option!)
+    else await randomChoose()
+    navigate('/game')
+  }
 
   return (
     <div className="flex flex-col items-center p-10 mt-5 font-bold">
@@ -17,21 +26,21 @@ export default function Play() {
       <div className="relative w-96 h-96">
         <ImageTooltip
           tooltipText="Rock"
-          onClick={() => chooseOption('rock')}
+          onClick={() => handleSubmit('rock')}
           className="absolute top-0 left-1/2 transform -translate-x-1/2 w-48 h-48"
         >
           <RockIcon />
         </ImageTooltip>
         <ImageTooltip
           tooltipText="Paper"
-          onClick={() => chooseOption('paper')}
+          onClick={() => handleSubmit('paper')}
           className="absolute bottom-0 left-0 transform -translate-x-20 translate-y-20 w-48 h-48"
         >
           <PaperIcon />
         </ImageTooltip>
         <ImageTooltip
           tooltipText="Scissors"
-          onClick={() => chooseOption('scissors')}
+          onClick={() => handleSubmit('scissors')}
           className="absolute bottom-0 right-0 transform translate-x-20 translate-y-20 w-48 h-48"
         >
           <ScissorsIcon />
@@ -46,7 +55,7 @@ export default function Play() {
         ) : (
           <div className="flex flex-col items-center gap-3">
             <span>Or ...</span>
-            <Button onClick={randomChoose}>
+            <Button onClick={() => handleSubmit(undefined, true)}>
               Try your luck
               <CloverIcon />
             </Button>

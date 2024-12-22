b8a9ef71329e4294a6f6d22ecc9cd74d406a031e
Lucas Franco <lcs.franco556@gmail.com>
2024-12-21 21:10:10 -0300

    feat: footer finished

--- src/ui/components/Footer.tsx
+++ src/ui/components/Footer.tsx
@@ -31,10 +31,29 @@ export default function Footer() {
         </DialogTrigger>
         <DialogContent>
           <DialogHeader>
-            <DialogTitle>Are you absolutely sure?</DialogTitle>
-            <DialogDescription>
-              This action cannot be undone. This will permanently delete your
-              account and remove your data from our servers.
+            <DialogTitle className="text-2xl">Game Rules</DialogTitle>
+            <DialogDescription className="space-y-4 text-xl">
+              <p>
+                Players must simultaneously stretch out their hands, each
+                forming a symbol (representing rock, paper, or scissors).
+              </p>
+              <p>
+                Then, players compare their symbols to determine the winner as
+                follows:
+              </p>
+              <ul className="list-disc pl-6 space-y-2">
+                <li>Rock beats scissors (crushing or breaking them).</li>
+                <li>Scissors beat paper (cutting it).</li>
+                <li>Paper beats rock (wrapping it).</li>
+              </ul>
+              <p>
+                If both players make the same gesture, it's a tie, and they
+                usually play again until there is a winner.
+              </p>
+              <p className="font-bold">
+                This game has one single rule: it's not allowed to show rock
+                twice in a row.
+              </p>
             </DialogDescription>
           </DialogHeader>
         </DialogContent>

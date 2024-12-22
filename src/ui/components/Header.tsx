988acca57aa0f700dffbc08e6a9c7c18beb86401
Lucas Franco <lcs.franco556@gmail.com>
2024-12-21 21:15:47 -0300

    refactor: better title

--- src/ui/components/Header.tsx
+++ src/ui/components/Header.tsx
@@ -4,11 +4,9 @@ export default function Header() {
   const { score } = useGameProvider()
 
   return (
-    <div className="flex justify-between p-4 text-xl">
-      <div>
-        <h3>Pedra</h3>
-        <h3>Papel</h3>
-        <h3>Tesoura</h3>
+    <div className="flex justify-between p-4 text-xl items-center">
+      <div className="ml-4">
+        <h1 className="font-extrabold text-4xl">Jo Ken Pô</h1>
       </div>
       <div className="flex flex-col px-8 rounded-2xl">
         <h3 className="">Score</h3>

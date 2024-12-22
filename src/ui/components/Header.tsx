a2068a1c83c33765d99d7699f916fb813228504b
Lucas Franco <lcs.franco556@gmail.com>
2024-12-21 21:34:07 -0300

    feat: add card component to header

--- src/ui/components/Header.tsx
+++ src/ui/components/Header.tsx
@@ -1,7 +1,8 @@
 import { useGameProvider } from '@app/contexts/GameProvider'
+import { Card, CardContent, CardHeader, CardTitle } from './ui/Card'
 
 export default function Header() {
-  const { score } = useGameProvider()
+  const { playerScore, machineScore } = useGameProvider()
 
   return (
     <div className="flex justify-between p-4 text-xl items-center">
@@ -9,8 +10,15 @@ export default function Header() {
         <h1 className="font-extrabold text-4xl">Jo Ken Pô</h1>
       </div>
       <div className="flex flex-col px-8 rounded-2xl">
-        <h3 className="">Score</h3>
-        <h1 className="justify-center flex font-extrabold text-3xl">{score}</h1>
+        <Card>
+          <CardHeader className="items-center">
+            <CardTitle>Score</CardTitle>
+          </CardHeader>
+          <CardContent>
+            <p>Player: {playerScore} </p>
+            <p>Machine: {machineScore} </p>
+          </CardContent>
+        </Card>
       </div>
     </div>
   )
